2b4af19f2fe7578727fab4bf971ed72b7d080397
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-05-18 20:04:08 -0300

    Adiciona Validações para Users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { BadRequestError } from "../helpers/apiError";
 import { UserService } from "../services/UserService";
 import { Request, Response, NextFunction } from "express";
 
@@ -9,11 +10,12 @@ export class UserController{
     createUser = async(req: Request, res: Response, next: NextFunction)=>{
         try {
             const{name, lastName, email} = req.body;
+            await this.userService.validateSchema(req.body)
             console.log(name, lastName, email);
             const newUser = await this.userService.create(name,lastName, email);
             return res.status(200).json(newUser);
             
-        } catch (error) {
+        } catch (error: unknown) {
            next(error); 
         }
         
@@ -22,7 +24,7 @@ export class UserController{
         try {
             const users = await this.userService.listAll();
       return res.json(users);
-        } catch (error) {
+        } catch (error: unknown) {
             next(error);
         }
     };
@@ -31,22 +33,29 @@ export class UserController{
             const id = Number(req.params.id);
             await this.userService.delete(id);
             return res.status(204).send();
-        } catch (error) {
+        } catch (error: unknown) {
             next(error)
         }
     };
     updateUser = async(req: Request, res: Response, next: NextFunction)=>{
         try {
-            const userId = Number(req.params.id);
-            const user = await this.userService.update(userId, req.body);
+            const id = Number(req.params.id);
+            if (isNaN(id)) {
+                throw new BadRequestError("ID inválido");
+              }
+            await this.userService.validateSchema(req.body, true)
+            const user = await this.userService.update(id, req.body);
             return res.status(200).json(user);
-        } catch (error) {
+        } catch (error: unknown) {
             next(error)
         }
     };
     toggleActiveUser = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const id = Number(req.params.id);
+            if (isNaN(id)) {
+                throw new BadRequestError("ID inválido");
+              }
             const user = await this.userService.toggleActive(id);
             return res.json({
                 message: `Usuário ${
@@ -54,7 +63,7 @@ export class UserController{
                     }   com sucesso.`,
                     user,
                 });
-        } catch (error) {
+        } catch (error: unknown) {
             next(error)
         }
     };
